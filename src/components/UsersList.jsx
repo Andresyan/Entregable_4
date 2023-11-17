0a13b40c8706d5aef7c4f38741ac4d762a408e22
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-17 09:34:28 -0500

    Update1 UsersList.jsx

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -6,9 +6,9 @@ const UsersList = ({users, selectUser, deleteUser}) => {
             {users.map((user)=> (
                 <ul key={user.id}>
                     <div>
-                    <li>{user.first_name} {user.last_name}</li>
-                    <li>{user.email}</li>
-                    <li><i className="fa-solid fa-cake-candles"></i>{user.birthday}</li>
+                    <li>{user.name} {user.lastName}</li>
+                    <li>{user.dni}</li>
+                    <li>{user.occupation}</li>
                     </div>
                     <div className='card-button'>
                     <button onClick={()=>{deleteUser(user.id)}} ><i className="fa-solid fa-trash"></i></button>
