76edd3e9f818099ff863f8c8c51d1ae45601ac7b
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-24 09:45:59 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ function App() {
   useEffect(()=>{
     axios
       .get("https://tasks22.onrender.com/api/v1/users/")
-      .then((res) => setUsers(res.data));
+      .then((res) => setUsers(res.data.data));
   }, []);
 
   const getUsers = () => {
     axios
       .get("https://tasks22.onrender.com/api/v1/users/")
-      .then((res) => setUsers(res.data));
+      .then((res) => setUsers(res.data.data));
   };
 
   const selectUser = (user) => {
