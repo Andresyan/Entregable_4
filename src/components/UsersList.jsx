1597f449e06f0b58c8db2c7b6765c2ca7b088d37
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-24 14:47:49 -0500

    Update UsersList.jsx

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const UsersList = ({ usersList, selectUser, deleteUser }) => {
-  console.log('Users prop in UsersList:', usersList?.users?.[1]);
+  console.log('Users prop in UsersList:', usersList?.users?.[1].name);
   return (
     <div className="card-user">
       <h1>Lista usuarios</h1>
