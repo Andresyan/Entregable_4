220ad5e02d572aee9228245c1ffa2d399dba4215
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-24 10:47:59 -0500

    Update UsersList.jsx

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -6,7 +6,7 @@ const UsersList = ({users, selectUser, deleteUser}) => {
       <h1>Lista usuarios</h1>
       console.log(users);
       <ul>
-        {users.data.map((user) => (
+        {users.data?.map((user) => (
           <li key={user.id}>
             <h4>{user.id}</h4>
             <div>
