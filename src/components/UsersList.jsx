aa82566d586b760c275a729c93b38aa3b468c0a3
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-24 11:52:20 -0500

    Update UsersList.jsx

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -5,23 +5,7 @@ const UsersList = ({ usersList, selectUser, deleteUser }) => {
   return (
     <div className="card-user">
       <h1>Lista usuarios</h1>
-      <ul>
-        {usersList.map((user) => (
-          <li key={user.id}>
-            <h4>{user.id}</h4>
-            <div>
-              <b>name: </b>
-              {user.name}
-            </div>
-            <div>
-              <b>dni: </b>
-              {user.dni}
-            </div>
-            <button onClick={() => deleteUser(user)}>Delete</button>
-            <button onClick={() => selectUser(user)}>Select</button>
-          </li>
-        ))}
-      </ul>
+      {userList.1}
     </div>
   );
 };
