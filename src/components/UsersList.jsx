0a7a918f3950c6defbd987b78c898d6f0ba48161
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-24 11:41:39 -0500

    Update UsersList.jsx

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -6,7 +6,7 @@ const UsersList = ({ users, selectUser, deleteUser }) => {
     <div className="card-user">
       <h1>Lista usuarios</h1>
       <ul>
-        {Array.isArray(users.data.data.users) && users.data.data.users.map((user) => (
+        {Array.isArray(users.data.data) && users.data.data.map((user) => (
           <li key={user.id}>
             <h4>{user.id}</h4>
             <div>
