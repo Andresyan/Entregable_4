f735bfeb8dc37c8984f9fb3093f7ad2402b3f0b8
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-24 10:36:12 -0500

    Update UsersList.jsx

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -2,12 +2,30 @@ import React from 'react';
 
 const UsersList = ({users, selectUser, deleteUser}) => {
     return (
-        <div className='card-user'>
-            {users.map((user)=>(user.name))}
-            console.log(user);
-            
-        </div>
-    );
+    <div className="card-user">
+      <h1>Lista usuarios</h1>
+      console.log(users);
+      <ul>
+        {productsList.map((user) => (
+          <li key={user.id}>
+            <h4>{user.id}</h4>
+            <div>
+              <b>name: </b>
+              {user.name}
+            </div>
+            <div>
+              <b>dni: </b>
+              {user.dni}
+            </div>
+            <button onClick={() => deleteUser(user)}>Delete</button>
+            <button onClick={() => selectUser(user)}>Select</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default UsersList;
+
+
