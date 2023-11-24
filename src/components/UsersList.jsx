8b33979bd5b70c70c72d4dd1ca5c50b1df3c12d5
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-24 12:03:52 -0500

    Update UsersList.jsx

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -5,7 +5,7 @@ const UsersList = ({ usersList, selectUser, deleteUser }) => {
   return (
     <div className="card-user">
       <h1>Lista usuarios</h1>
-      {usersList}
+      {usersList.users}
     </div>
   );
 };
