fd9c9c0cf04ab549fa0efc33efd49a5805d8c493
andres yance <104406112+Andresyan@users.noreply.github.com>
2023-11-24 14:36:53 -0500

    Update UsersList.jsx

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,7 +4,7 @@ const UsersList = ({ usersList, selectUser, deleteUser }) => {
   console.log('Users prop in UsersList:', usersList);
   return (
     <div className="card-user">
-      <h1>Lista usuarios {usersList}</h1>
+      <h1>Lista usuarios</h1>
       
     </div>
   );
